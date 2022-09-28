6f27c3d7ea0dc71a4b2ffc1b29961ef8a07ac1ed
matg0mes <hotmus8500@gmail.com>
2022-09-28 00:33:08 -0300

    feat(app): multithread processing e integrate rabbitmq

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 
 	"github.com/matg0mes/go-first-project/internal/order/infra/database"
 	"github.com/matg0mes/go-first-project/internal/order/usecase"
+	"github.com/matg0mes/go-first-project/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	maxWorkers := 3
+	wg := sync.WaitGroup{}
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 	if err != nil {
 		panic(err)
@@ -15,16 +22,36 @@ func main() {
 	defer db.Close()
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
-
-	input := usecase.OrderInputDTO{
-		ID: "12345",
-		Price: 100,
-		Tax: 10,
-	}
-
-	output,err := uc.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
+	ch.Qos(100, 0, false)
 	if err != nil {
 		panic(err)
 	}
-	println(output.FinalPrice)
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+
+	wg.Add(maxWorkers)
+	for i := 0; i< maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, uc, i)
+	}
+	wg.Wait()
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerdId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+		input.Tax = 10.0
+		_, err = uc.Execute(input)
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+		msg.Ack(false)
+		fmt.Println("Worker", workerdId, "processed order", input.ID)
+	}
 }
